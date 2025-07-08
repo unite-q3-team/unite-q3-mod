062edef05b006cfe5277d24ea8aa5e63070be3a1
never__byte <218614228+unite-q3@users.noreply.github.com>
2025-07-08 17:45:01 +0300

    msec

--- code/game/cmds/serverutils.c
+++ code/game/cmds/serverutils.c
@@ -3,6 +3,6 @@
 
 void Cmd_svfps_f(gentity_t *ent) {
     // sv_fps.integer
-    trap_SendServerCommand(ent - g_entities, va("print \"^3Current server fps^1: ^2%d ^1[^2N/A^1]\n\"", sv_fps.integer));
+    trap_SendServerCommand(ent - g_entities, va("print \"^3Current server fps^1: ^2%d ^1[^2%.2f^1 msec]\n\"", sv_fps.integer, (1000.0f / sv_fps.value)));
     trap_SendServerCommand(ent - g_entities, va("print \"^3Level time time^1: ^2%d\n\"", level.time));
 }
